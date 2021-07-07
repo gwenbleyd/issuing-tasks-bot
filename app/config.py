4f14061024be9bb922508e336813528ba5f5cb0f
Andrew <53082952+gwenbleyd@users.noreply.github.com>
2021-07-08 00:53:24 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -11,10 +11,6 @@ TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 MAIN_CHAT_ID = os.getenv("MAIN_CHAT_ID")
 GROUP_ID = os.getenv("GROUP_ID")
-# TOKEN = '1804068546:AAGxAbE5ArBkl3hmlRn8YQfaSgtuqo7iL9Q'
-# CHAT_ID = '@test_for_bot_chan'
-# MAIN_CHAT_ID = 777000
-# GROUP_ID = -1001386327684
 
 bot = Bot(token=TOKEN)
 
@@ -22,6 +18,4 @@ logger = logging.getLogger(__name__)
 
 msg_task = types.message
 
-# msgs = list()
 SUPER_USER_ID = 447959709
-# add_admin(SUPER_USER_ID, 'gwenbleyd')
